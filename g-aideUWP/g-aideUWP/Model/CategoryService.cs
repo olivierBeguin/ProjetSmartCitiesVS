9cb81c53fd0fa3a2ca836d761e5f793f7243d4e8
louisvdd <etu28841@student.henallux.be>
2016-11-22 10:22:49 +0100

    sdf

---
+++ g-aideUWP/g-aideUWP/Model/CategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace g_aideUWP.Model
+{
+    class CategoryService
+    {
+        public long Id { get; set; }
+        public string Label { get; set; }
+    }
+}
---
+++ g-aideUWP/g-aideUWP/Model/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace g_aideUWP.Model
+{
+    class Service
+    {
+        public long Id { get; set; }
+        public string DescriptionService { get; set; }
+        public DateTime DatePublicationService { get; set; }
+        public CategoryService Category { get; set; }
+    }
+}
